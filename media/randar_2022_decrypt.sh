c2f8f669960c7a7947b9c62f507007a40a98d151
pcm1k <87557538+pcm1k@users.noreply.github.com>
2024-03-05 00:23:59 -0500

    put password directly in randar_2022_decrypt.sh

--- media/randar_2022_decrypt.sh
+++ media/randar_2022_decrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-openssl enc -d -a -aes-256-cbc -pbkdf2 -kfile randar_2022_password.txt < randar_2022.txt.enc
+openssl enc -d -a -aes-256-cbc -pbkdf2 -k '7L(dhv*s]7Z5-B&j~D]H8HG)5YPcXTi*' < randar_2022.txt.enc
