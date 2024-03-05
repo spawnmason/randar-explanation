938a590e5d31fe5e94219aafdafa87fd1dd3fed4
pcm1k <87557538+pcm1k@users.noreply.github.com>
2024-03-05 14:13:50 -0500

    rename "randar_2022" to "randar_n0p_2022"

--- media/randar_2022_decrypt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-openssl enc -d -a -aes-256-cbc -pbkdf2 -k '7L(dhv*s]7Z5-B&j~D]H8HG)5YPcXTi*' < randar_2022.txt.enc
---
+++ media/randar_n0p_2022_decrypt.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+openssl enc -d -a -aes-256-cbc -pbkdf2 -k '7L(dhv*s]7Z5-B&j~D]H8HG)5YPcXTi*' < randar_n0p_2022.txt.enc
